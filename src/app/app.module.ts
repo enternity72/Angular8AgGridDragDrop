a46306a150719971e073d5c8180ed698173d9940
PavelKushnirchuk <Pavlo_Kushnirchuk@epam.com>
2020-04-16 15:37:53 +0300

    Added code from example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
+
+// import {ModuleRegistry} from '@ag-grid-community/core';     // @ag-grid-community/core will always be implicitly available
+// import {ClientSideRowModelModule} from "@ag-grid-community/client-side-row-model";
+// ModuleRegistry.registerModules([
+//   ClientSideRowModelModule
+// ]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
